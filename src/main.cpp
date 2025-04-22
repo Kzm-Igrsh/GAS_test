4c15413f8185e7a91661237344ae6dbb44e8b779
Kzm-Igrsh <50rashi-1ma@ifdl.jp>
2025-04-22 14:17:59 +0900

    fuuuuuuuuuuu_sample

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ArduinoJson.h>
 
 // ref: https://sample.msr-r.net/m5stack-line-notify/
-//fja;lfjasdgjeoaasdkjfasdjsdfj;//
+//fuuuuuuuuuuuuuuuuuuuuuu//
 
 const char* ssid     = "ifdl"; // WiFi SSID
 const char* password = "hogeupip5"; // WiFi Password
